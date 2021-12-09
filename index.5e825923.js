9f95ea80651d8d6180720495146b68f7d620278b
ManzDev <JoseRomanHernandez@gmail.com>
2021-12-09 02:09:30 +0000

    Updates

---
+++ index.5e825923.js
@@ -0,0 +1,170 @@
+const d=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=l(e);fetch(e.href,r)}};d();var c='<svg width="332.463" height="479.954" viewBox="0 0 87.964 126.988" xmlns="http://www.w3.org/2000/svg"><g class="container"><g style="stroke:#fff;stroke-width:8;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" class="outline-group"><path d="M20.55 54.151C.587 42.047 6.81 30.468 6.81 30.468M71.258 54.151C91.223 42.047 85 30.468 85 30.468M51.396 98.34c4.052 16.704-.76 24.67-.76 24.67h7.597M36.942 98.34c-4.05 16.704.76 24.67.76 24.67h-7.596" style="fill:none;stroke:#fff;stroke-width:8;stroke-linecap:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" transform="translate(-1.922 -.023)"/><path style="stroke:#fff;stroke-width:7.99999;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" class="outer" d="M44.629 4.023a27.417 37.564 0 0 0-.755.011 27.417 37.564 0 0 0-27.417 37.564 27.417 37.564 0 0 0 0 6.261 34.271 34.781 0 0 0-6.168 20.87 34.271 34.781 0 0 0 33.585 34.085 34.271 34.781 0 0 0 34.272-34.086 34.271 34.781 0 0 0-6.854-20.869 27.417 37.564 0 0 0 .685-6.26A27.417 37.564 0 0 0 44.629 4.022z" transform="translate(-1.922 -.023)"/></g><g class="arms-group"><path d="M20.55 54.151C.587 42.047 6.81 30.468 6.81 30.468" class="arm-left" style="fill:none;stroke:#000;stroke-width:3.79103;stroke-linecap:round" transform="translate(-1.922 -.023)"/><path d="M71.258 54.151C91.223 42.047 85 30.468 85 30.468" class="arm-right" style="fill:none;stroke:#000;stroke-width:3.79103;stroke-linecap:round" transform="translate(-1.922 -.023)"/></g><g class="legs-group"><path d="M51.396 98.34c4.052 16.704-.76 24.67-.76 24.67h7.597" class="leg-right" style="fill:none;stroke:#000;stroke-width:3.79103;stroke-linecap:round" transform="translate(-1.922 -.023)"/><path d="M36.942 98.34c-4.05 16.704.76 24.67.76 24.67h-7.596" class="leg-left" style="fill:none;stroke:#000;stroke-width:3.79103;stroke-linecap:round" transform="translate(-1.922 -.023)"/></g><g class="body-group"><path class="body" fill="#2a670c" d="M43.875 4.034a27.417 37.564 0 0 0-27.418 37.564 27.417 37.564 0 0 0 0 6.261 34.271 34.781 0 0 0-6.168 20.869 34.271 34.781 0 0 0 33.586 34.086 34.271 34.781 0 0 0 34.27-34.086 34.271 34.781 0 0 0-6.853-20.869 27.417 37.564 0 0 0 .685-6.26A27.417 37.564 0 0 0 43.875 4.033z" style="stroke-width:1.36134" transform="translate(-1.922 -.023)"/><path class="body-inner" fill="#c1e20c" d="M43.875 8.904a24.675 33.39 0 0 0-24.676 33.39 24.675 33.39 0 0 0 .686 5.565 30.159 30.608 0 0 0-6.17 18.086 30.159 30.608 0 0 0 30.16 30.608 30.159 30.608 0 0 0 30.844-30.608 30.159 30.608 0 0 0-6.17-18.086 24.675 33.39 0 0 0 0-5.565 24.675 33.39 0 0 0-24.674-33.39z" style="stroke-width:1.36134" transform="translate(-1.922 -.023)"/></g><g class="seed-group" transform="translate(-1.922 -.023)"><ellipse cx="44.56" cy="65.25" fill="#452c0c" class="seed" style="stroke-width:1.36134" rx="13.709" ry="13.913"/><path fill="#FFF2" d="M43.875 74.989a10.281 10.434 0 0 1-8.911-4.87 10.281 10.434 0 0 1 0-10.434 10.281 10.434 0 0 1 8.91-4.87" class="seed-inner" style="stroke-width:.947758"/></g><g class="face-group" transform="translate(-1.922 -.023)"><ellipse cx="39.077" cy="29.773" class="eye-left" style="stroke-width:.947758" rx="4.113" ry="4.174"/><ellipse cx="37.706" cy="29.077" fill="#fff" class="pupil-left" style="stroke-width:1.36134" rx="2.056" ry="2.087"/><ellipse cx="49.358" cy="29.773" class="eye-right" style="stroke-width:.947758" rx="4.113" ry="4.174"/><ellipse cx="48.673" cy="29.077" fill="#fff" class="pupil-right" style="stroke-width:1.36134" rx="2.056" ry="2.087"/><path d="M49.358 37.425a5.483 5.565 0 0 1-2.742 4.869 5.483 5.565 0 0 1-5.483 0 5.483 5.565 0 0 1-2.742-4.87" class="mouth" style="stroke-width:.947758"/></g></g></svg>',p='<svg width="591.796" height="704.655" viewBox="0 0 156.579 186.44" xmlns="http://www.w3.org/2000/svg"><g class="body-group"><circle style="fill:#fff;fill-opacity:1;stroke:#fff;stroke-width:6.74721;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers;stop-color:#000" cx="78.29" cy="108.15" r="74.916" class="body"/><path style="fill:#000;fill-opacity:1;stroke:#fff;stroke-width:3.37361;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers;stop-color:#000" d="M44.919 41.111a74.916 74.916 0 0 0-41.545 67.04 74.916 74.916 0 0 0 .704 9.988 60.527 44.294 0 0 0 .45.033 60.527 44.294 0 0 0 60.527-44.294A60.527 44.294 0 0 0 44.919 41.11zm84.653 12.434a34.184 28.22 0 0 0-2.492 9.95 34.184 28.22 0 0 0 23.958 26.786 74.916 74.916 0 0 0-21.466-36.736zm-5.204 60.339a31.697 31.994 0 0 0-20.478 7.602 19.64 19.825 0 0 0-10.17-2.867 19.64 19.825 0 0 0-19.64 19.825 19.64 19.825 0 0 0 19.64 19.825 19.64 19.825 0 0 0 1.424-.053 31.697 31.994 0 0 0 16.721 16.866 74.916 74.916 0 0 0 38.588-46.997 31.697 31.994 0 0 0-26.085-14.201zM32.333 131.88a43.536 60.093 0 0 0-21.739 8.321 74.916 74.916 0 0 0 64.766 42.756 43.536 60.093 0 0 0-43.027-51.077z" class="body-stain"/></g><g class="head"><g class="ears"><path style="fill:#fbaba9;fill-opacity:1;stroke:#000;stroke-width:3.75823;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="M94.003 15.884s12.469-4.812 18.996-5.067c12.121-.474 20.598 5.55 20.598 5.55s-.458 10.617-12.817 17.614c-9.109 5.158-26.777-18.097-26.777-18.097" class="ear-right"/><path style="fill:#fbaba9;fill-opacity:1;stroke:#fff;stroke-width:3.75823;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="m67.122 25.366-18.897 5.462-22.987-6.084s.68-14.551 14.772-16.913C50.222 6.12 67.122 25.366 67.122 25.366" class="ear-left"/></g><g class="horns"><rect style="fill:gold;fill-opacity:1;stroke:none;stroke-width:3.02673;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers;stop-color:#000" width="19.1" height="18.483" x="51.233" y="-19.391" ry="4.201" rx="4.476" transform="matrix(.94173 .33636 -.30592 .95206 0 0)" class="horn-left"/><rect style="fill:gold;fill-opacity:1;stroke:none;stroke-width:3.02673;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers;stop-color:#000" width="19.1" height="18.483" x="-102.664" y="34.981" ry="4.201" rx="4.476" transform="matrix(-.94173 .33636 .30592 .95206 0 0)" class="horn-right"/></g><g class="face"><path style="fill:#fffefe;fill-opacity:1;stroke:none;stroke-width:3.92768;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers;stop-color:#000" d="M69.208 4.065c-17.39 0-31.39 14.76-31.39 33.094v51.776h83.03V37.159c0-18.334-14-33.094-31.39-33.094z" class="head"/><path d="M52.011 96.907c-4.652-.176-6.136-.365-7.716-.984-5.669-2.219-9.751-5.96-12.08-11.07-1.774-3.893-2.396-6.612-2.396-10.473 0-10.504 6.64-19.645 15.988-22.012 3.194-.81 62.07-.81 65.264 0 9.426 2.387 16 11.375 15.983 21.854-.016 10.366-5.483 18.42-14.903 21.958-1.319.495-5.094.598-27.952.76-14.535.102-29.02.087-32.188-.033z" style="fill:#f9aaa8;stroke-width:.0929095" class="snout"/><path d="M59.908 43.472c-5.776-1.163-8.595-8.347-5.287-13.474 3.281-5.084 10.414-5.084 13.694 0 4.083 6.328-1.273 14.911-8.407 13.474zm33.909 0c-5.776-1.163-8.595-8.347-5.287-13.474 3.281-5.084 10.414-5.084 13.694 0 4.083 6.328-1.273 14.911-8.407 13.474z" style="fill:#101010;stroke-width:.0243737" class="eyes"/></g></g></svg>',s=[{date:"15/MAY/2021",title:"Avocado",rating:"5.0",tags:["canjeable","emote"],text:"Es el personaje m\xE1s famoso del canal, al que desde ni\xF1o le hicieron creer que era una verdura. Ahora, tras una mordida de Michael Jackson, es una paltita que se dedica a bailar en los streams junto a unos doritos.",link:"https://youtube.com/c/ManzDev"},{date:"4/DIC/2021",title:"Spherical Cow",rating:"5.0",tags:["emote"],text:"Una vaca sagrada que aprendi\xF3 a rodar para huir de los OVNIS. Lo aprendi\xF3 de BB-8. Su complexi\xF3n hace que sea sencillo huir de los enemigos, no obstante, se puede encontrar por el canal rodando alegremente.",link:"https://youtube.com/c/ManzDev"}];const f=[c,p],h=o=>o[0].toUpperCase()+o.substring(1);class n extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"})}static get styles(){return`
+      :host {
+        --width: 325px;
+        --title-font: "Encode Sans", sans-serif;
+        --normal-font: "Clear Sans", sans-serif;
+        --bottom-gradient: linear-gradient(#22213E00 20%, #000 40%);
+        --top-gradient: repeating-conic-gradient(at 49% 38%, #fff4 5%, #0000 15%, #fff4 17%);
+        font-size: 16px;
+      }
+
+      .container {
+        width: var(--width);
+        height: var(--height);
+        min-width: 250px;
+        margin: 25px;
+        margin-top: 6em;
+        border-radius: 25px;
+        box-shadow: 2px 2px 12px 4px #0006;
+        padding: 25px;
+        background:
+          var(--bottom-gradient),
+          var(--top-gradient);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        animation: move-bg 5s ease-in-out infinite alternate;
+        transition: background 5s ease-in-out;
+        position: relative;
+      }
+
+      .image {
+        height: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+      }
+
+      .image svg {
+        max-height: 300px;
+        transform: translateY(18px);
+        clip-path: polygon(0 0, 100% 0, 100% 94%, 0% 94%);
+        filter: drop-shadow(0 0 10px #0007);
+      }
+
+      .image svg .container {
+        transform: translateY(8px);
+      }
+
+      .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 55%;
+        padding: 15px;
+        box-sizing: border-box;
+        font-family: var(--normal-font);
+      }
+
+      h2.title {
+        font-family: var(--title-font);
+        font-size: 1.4rem;
+      }
+
+      .date {
+        color: #aaa;
+        font-weight: bold;
+        font-size: 0.8rem;
+        letter-spacing: 2px;
+      }
+
+      .title {
+        color: #fff;
+      }
+
+      .info {
+        display: flex;
+        align-items: center;
+        color: #fff;
+      }
+
+      .star {
+        color: #F8CB1C;
+      }
+
+      .tags {
+        display: flex;
+        gap: 6px;
+        margin: 0 10px;
+        user-select: none;
+      }
+
+      .tags .tag {
+        background: #3D3D3D;
+        border-radius: 5px;
+        padding: 5px 10px;
+        color: #aaa;
+        letter-spacing: -1px;
+        font-size: 0.8rem;
+      }
+
+      .text {
+        color: #888;
+        text-align: center;
+        letter-spacing: -0.5px;
+        margin-bottom: 1em;
+      }
+
+      .button {
+        display: flex;
+        width: 100%;
+        background: #5D2D9D;
+        color: #fff;
+        text-decoration: none;
+        display: flex;
+        justify-content: center;
+        padding: 10px;
+        border-radius: 5px;
+        transition: background 0.5s;
+        margin-bottom: 0.25em;
+      }
+
+      .arrow {
+        position: absolute;
+        top: 6px;
+        color: #fff;
+        font-size: 2.25rem;
+        text-shadow:
+          0 0 3px #000,
+          2px 3px 4px #0007;
+        cursor: pointer;
+      }
+
+      .arrow.left {
+        left: 20px;
+        transform: scaleX(-1);
+      }
+
+      .arrow.right {
+        right: 20px;
+      }
+
+      .button:hover {
+        background: #8B43EA;
+      }
+    `}static get observedAttributes(){return["index"]}attributeChangedCallback(t,l,a){t==="index"&&(this.index=parseInt(a),this.render())}connectedCallback(){this.index=Number(this.getAttribute("index"))||0,this.render()}prev(){const t=Math.max(0,this.index-1);this.setAttribute("index",t)}next(){const t=Math.min(s.length-1,this.index+1);this.setAttribute("index",t)}getTags(){return s[this.index].tags.map(t=>`<span class="tag">${h(t)}</span>`).join("")}setPrevArrow(){if(this.index===0)return;const t=document.createElement("div");t.classList.add("left","arrow"),t.textContent="\u2794",t.addEventListener("click",()=>this.prev()),this.shadowRoot.querySelector(".container").appendChild(t)}setNextArrow(){if(this.index===s.length-1)return;const t=document.createElement("div");t.classList.add("right","arrow"),t.textContent="\u2794",t.addEventListener("click",()=>this.next()),this.shadowRoot.querySelector(".container").appendChild(t)}render(){this.shadowRoot.innerHTML=`
+    <style>${n.styles}</style>
+    <div class="container">
+      <div class="image">
+        ${f[this.index]}
+      </div>
+      <div class="content">
+        <div class="date">${s[this.index].date}</div>
+        <h2 class="title">${s[this.index].title}</h2>
+        <div class="info">
+          <div class="rating">
+            ${s[this.index].rating}
+            <span class="star">\u2605</span>
+          </div>
+          <div class="tags">
+            ${this.getTags()}
+          </div>
+        </div>
+        <div class="text">
+          <p>${s[this.index].text}</p>
+        </div>
+        <a class="button" href="${s[this.index].link}">
+          Ver su primera aparici\xF3n
+        </a>
+      </div>
+    </div>`,this.hasAttribute("static")||(this.setPrevArrow(),this.setNextArrow())}}customElements.define("manzdev-memecard",n);
